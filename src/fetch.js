d1b49e5a09b783b6fc1bd0e3c512857e7b1812f0
MaksymilianBilski <biger9@o2.pl>
2022-10-26 05:56:35 +0200

    fetch/button fetchmore/async/little bit of css/creating gallery

--- src/fetch.js
+++ src/fetch.js
@@ -1,12 +1,27 @@
 const axios = require('../node_modules/axios/dist/axios/');
-const URL = 'https://pixabay.com/api/?key=';
 const KEY = '30839127-8a41b37b8b94b94b2633e44b5';
+const imageType = 'photo';
+const imageOrientation = '&horizontal';
+const safesearch = 'true';
+const URL =
+  'https://pixabay.com/api/?key=' +
+  KEY +
+  '&image_type=' +
+  imageType +
+  '&image_orientation=' +
+  imageOrientation +
+  '&safesearch=' +
+  safesearch;
+const fetchBtn = document.querySelector('.fetch-button');
+let page = 1;
 
 async function fetchData(inputValue) {
   try {
-    const response = await axios.get(`${URL + KEY + '&q=' + inputValue}`);
+    const response = await axios.get(
+      `${URL + '&q=' + inputValue + '&page=' + page}`
+    );
     const data = await response.data;
-    console.log(data);
+    return data;
   } catch (error) {
     console.log(error);
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,62 @@
 const form = document.querySelector('search-form#search-form');
 const input = document.querySelector('input[type=text]');
 const button = document.querySelector('button[type=submit]');
-import { fetchData } from './fetch';
-fetchData('flower');
+const gallery = document.querySelector('.gallery');
+const fetchBtn = document.querySelector('.fetch-button');
+import { fetchData, page } from './fetch';
+const newArr = [];
+
+function getData() {
+  return fetchData('flower').then(response => {
+    const dataArray = response.hits;
+    for (item of dataArray) {
+      newArr.push(item);
+    }
+  });
+}
+
+function createMarkup() {
+  for (item of newArr) {
+    gallery.insertAdjacentHTML(
+      'afterbegin',
+      `<div class="photo-card">
+        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width="420" />
+        <div class="info">
+          <p class="info-item">
+            <b>LIKES: ${item.likes}</b>
+          </p>
+          <p class="info-item">
+            <b>VIEWS: ${item.views}</b>
+          </p>
+          <p class="info-item">
+            <b>COMMENTS: ${item.comments}</b>
+          </p>
+          <p class="info-item">
+            <b>DOWNLOADS: ${item.downloads}</b>
+          </p>
+        </div>
+      </div>`
+    );
+  }
+}
+
+async function createGallery() {
+  try {
+    await getData();
+    createMarkup();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function cleanGallery() {
+  gallery.innerHTML = '';
+}
+
+fetchBtn.addEventListener('click', () => {
+  createGallery();
+  page += 1;
+  if (gallery.children.length > 1) {
+    gallery.innerHTML = '';
+  }
+});
