eb7e72f4419cd7e8dcc36ddcc8a5ec97bf00c96b
MaksymilianBilski <biger9@o2.pl>
2022-11-11 21:13:22 +0100

    druga metoda

--- src/fetch.js
+++ src/fetch.js
@@ -1,32 +1,52 @@
-const axios = require('../node_modules/axios/dist/axios/');
-import { page } from './index';
-const KEY = '30839127-8a41b37b8b94b94b2633e44b5';
-const imageType = 'photo';
-const imageOrientation = 'horizontal';
-const safesearch = 'true';
-const perPage = 20;
-const URL =
-  'https://pixabay.com/api/?key=' +
-  KEY +
-  '&image_type=' +
-  imageType +
-  '&image_orientation=' +
-  imageOrientation +
-  '&safesearch=' +
-  safesearch +
-  '&per_page=' +
-  perPage;
+// const axios = require('../node_modules/axios/dist/axios/');
+// import { page } from './index';
+// const KEY = '30839127-8a41b37b8b94b94b2633e44b5';
+// const imageType = 'photo';
+// const imageOrientation = 'horizontal';
+// const safesearch = 'true';
+// const perPage = 20;
+// const URL =
+//   'https://pixabay.com/api/?key=' +
+//   KEY +
+//   '&image_type=' +
+//   imageType +
+//   '&image_orientation=' +
+//   imageOrientation +
+//   '&safesearch=' +
+//   safesearch +
+//   '&per_page=' +
+//   perPage;
 
-async function fetchData(inputValue) {
-  try {
-    const response = await axios.get(
-      `${URL + '&q=' + inputValue + '&page=' + page}`
-    );
-    const data = await response.data;
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+// async function fetchData(inputValue) {
+//   try {
+//     const response = await axios.get(
+//       `${URL + '&q=' + inputValue + '&page=' + page}`
+//     );
+//     const data = await response.data;
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// export { fetchData, perPage };
+import {input } from './index';
+const URL = 'https://pixabay.com/api/?';
+
+function fetchPhotos(value) {
+  value = input.value;
+  const searchParams = new URLSearchParams({
+    key: '30839127-8a41b37b8b94b94b2633e44b5',
+    q: value,
+    safesearch: true,
+    orientation: 'horizontal',
+    image_type: 'photo',
+    page: 1,
+    per_page: 40,
+  });
+  return fetch(URL + searchParams)
+    .then(response => response.json())
+    .catch(error => error);
 }
 
-export { fetchData, perPage };
+export { fetchPhotos };
--- src/index.js
+++ src/index.js
@@ -1,108 +1,149 @@
-const form = document.querySelector('#search-form');
+// const form = document.querySelector('#search-form');
+// const input = document.querySelector('input[type=text]');
+// const button = document.querySelector('button[type=submit]');
+// const gallery = document.querySelector('.gallery');
+// const fetchBtn = document.querySelector('.load-more');
+// const photo = document.querySelector('.photo-card');
+
+// import { fetchData, perPage } from './fetch';
+// import { Notify } from 'notiflix';
+
+// let newArr = [];
+// let page = 0;
+// let total = 0;
+
+// fetchBtn.style.display = 'none';
+
+// //creating an array of images data
+// function getData() {
+//   const inputValue = input.value;
+//   return fetchData(inputValue).then(response => {
+//     const dataArray = response.hits;
+//     total = response.total;
+//     for (const item of dataArray) {
+//       newArr.push(item);
+//     }
+//   });
+// }
+
+// //creating each of data-images markups
+// async function createMarkup() {
+//   for (const item of newArr) {
+//     gallery.insertAdjacentHTML(
+//       'beforeend',
+//       `<div class="photo-card">
+//         <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width="420" height="420" />
+//         <div class="info">
+//           <p class="info-item">
+//             <b>LIKES: ${item.likes}</b>
+//           </p>
+//           <p class="info-item">
+//             <b>VIEWS: ${item.views}</b>
+//           </p>
+//           <p class="info-item">
+//             <b>COMMENTS: ${item.comments}</b>
+//           </p>
+//           <p class="info-item">
+//             <b>DOWNLOADS: ${item.downloads}</b>
+//           </p>
+//         </div>
+//       </div>`
+//     );
+//   }
+// }
+
+// //creating gallery
+// async function createGallery() {
+//   try {
+//     await getData();
+//     await createMarkup();
+//     if (total >= 1) {
+//       fetchBtn.style.display = 'block';
+//     }
+//     if (total <= 1) {
+//       fetchBtn.style.display = 'none';
+//     }
+//     if (total > 0 && page < 2) {
+//       setTimeout(() => {
+//         Notify.success(`Hooray! We found ${total} images.`);
+//       }, 250);
+//     }
+//     if (total === 0) {
+//       Notify.failure(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+//   fetchBtn.style.visibility = 'visible';
+//   if (gallery.children.length >= 1) {
+//     gallery.innerHTML = '';
+//     newArr = [];
+//   }
+//   page = 1;
+//   createGallery();
+// });
+
+// fetchBtn.addEventListener('click', e => {
+//   e.preventDefault();
+//   if (gallery.children.length > 1) {
+//     gallery.innerHTML = '';
+//   }
+//   page += 1;
+//   createGallery();
+//   let totalPages = total / perPage;
+//   if (page > totalPages) {
+//     fetchBtn.style.visibility = 'hidden';
+//     Notify.failure(
+//       "We're sorry, but you've reached the end of search results."
+//     );
+//   }
+// });
+
+// export { page };
+
+import { fetchPhotos } from './fetch';
+
 const input = document.querySelector('input[type=text]');
+const form = document.querySelector('form');
 const button = document.querySelector('button[type=submit]');
 const gallery = document.querySelector('.gallery');
-const fetchBtn = document.querySelector('.load-more');
-const photo = document.querySelector('.photo-card');
+let value = input.value;
 
-import { fetchData, perPage } from './fetch';
-import { Notify } from 'notiflix';
-
-let newArr = [];
-let page = 0;
-let total = 0;
-
-fetchBtn.style.display = 'none';
-
-//creating an array of images data
-function getData() {
-  const inputValue = input.value;
-  return fetchData(inputValue).then(response => {
-    const dataArray = response.hits;
-    total = response.total;
-    for (const item of dataArray) {
-      newArr.push(item);
-    }
-  });
-}
-
-//creating each of data-images markups
-async function createMarkup() {
-  for (const item of newArr) {
+function createGallery(data) {
+  return data.hits.forEach(hits =>
     gallery.insertAdjacentHTML(
-      'beforeend',
+      'afterbegin',
       `<div class="photo-card">
-        <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" width="420" height="420" />
-        <div class="info">
-          <p class="info-item">
-            <b>LIKES: ${item.likes}</b>
-          </p>
-          <p class="info-item">
-            <b>VIEWS: ${item.views}</b>
-          </p>
-          <p class="info-item">
-            <b>COMMENTS: ${item.comments}</b>
-          </p>
-          <p class="info-item">
-            <b>DOWNLOADS: ${item.downloads}</b>
-          </p>
-        </div>
-      </div>`
-    );
-  }
-}
-
-//creating gallery
-async function createGallery() {
-  try {
-    await getData();
-    await createMarkup();
-    if (total >= 1) {
-      fetchBtn.style.display = 'block';
-    }
-    if (total <= 1) {
-      fetchBtn.style.display = 'none';
-    }
-    if (total > 0 && page < 2) {
-      setTimeout(() => {
-        Notify.success(`Hooray! We found ${total} images.`);
-      }, 250);
-    }
-    if (total === 0) {
-      Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  <img src="${hits.webformatURL}" style="width: 420px; height: 420px" alt="${hits.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${hits.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${hits.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${hits.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${hits.downloads}</b>
+    </p>
+  </div>
+</div>`
+    )
+  );
 }
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  fetchBtn.style.visibility = 'visible';
-  if (gallery.children.length >= 1) {
-    gallery.innerHTML = '';
-    newArr = [];
-  }
-  page = 1;
-  createGallery();
+  fetchPhotos()
+    .then(data => createGallery(data))
+    .catch(error => console.log(error));
 });
-
-fetchBtn.addEventListener('click', e => {
-  e.preventDefault();
-  if (gallery.children.length > 1) {
-    gallery.innerHTML = '';
-  }
-  page += 1;
-  createGallery();
-  let totalPages = total / perPage;
-  if (page > totalPages) {
-    fetchBtn.style.visibility = 'hidden';
-    Notify.failure(
-      "We're sorry, but you've reached the end of search results."
-    );
-  }
-});
-
-export { page };
+export { value, input };
